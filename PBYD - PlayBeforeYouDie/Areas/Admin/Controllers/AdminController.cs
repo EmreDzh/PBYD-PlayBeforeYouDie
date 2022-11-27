1bbc50117db8f1ecd96da54ab583a986d89829bd
Emre Dzhebir <100286779+EmreDzh@users.noreply.github.com>
2022-11-27 18:26:37 +0100

    Added the Admin Area for the App

---
+++ PBYD - PlayBeforeYouDie/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PBYD___PlayBeforeYouDie.Areas.Admin.Controllers
+{
+    public class AdminController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PBYD - PlayBeforeYouDie/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace PBYD___PlayBeforeYouDie.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Administrator")]
+    public class BaseController : Controller
+    {
+        
+    }
+}
