bc0bc14ed7e031a76c8fdad773bde169076e2913
Emre Dzhebir <100286779+EmreDzh@users.noreply.github.com>
2022-11-27 18:16:26 +0100

    Created the Default Admin Area

--- PBYD - PlayBeforeYouDie/Program.cs
+++ PBYD - PlayBeforeYouDie/Program.cs
@@ -67,10 +67,21 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.UseEndpoints(endpoints =>
+{
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+        );
+
+    endpoints.MapControllerRoute(
+        name: "areas",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+
+    endpoints.MapRazorPages();
+});
 
 app.MapRazorPages();
 
